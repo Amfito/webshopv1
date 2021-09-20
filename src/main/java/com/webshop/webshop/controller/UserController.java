361c6efab7bc037957fb7cfd698ee4f47769ef68
Amfito <elvinas.scirbinskas@gmail.com>
2021-09-20 18:26:11 +0300

    added spring security dependency, changed application properties to use live database. added user registration/login system backend

---
+++ src/main/java/com/webshop/webshop/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.webshop.webshop.controller;
+
+import com.webshop.webshop.model.User;
+import com.webshop.webshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+
+    @RequestMapping(value = "/process_register", method = RequestMethod.POST)
+    public String processRegistration(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+        return "Register_Succesful";
+    }
+}
